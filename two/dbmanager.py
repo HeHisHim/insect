97b5d481f856f71dcd9c51153e6f442842bdd074
yoyboy <534411590@qq.com>
2019-05-05 15:22:38 +0800

    Update dbmanager.py

--- two/dbmanager.py
+++ two/dbmanager.py
@@ -3,13 +3,12 @@ sys.path.append("..")
 
 import pymysql
 import hashlib
-
+import threading
 from logConfig import logger
 
 class CrawlDataBaseManager:
     DB_NAME = "insect"
     SERVER_IP = "127.0.0.1"
-
     def __init__(self):
         try:
             self.conn = pymysql.connect(
@@ -20,24 +19,41 @@ class CrawlDataBaseManager:
                 charset = "utf8"
             )
         except Exception as identifier:
+            logger.error("5")
             logger.error(identifier)
             exit(1)
 
     # url入库
     def enqueueUrl(self, url, depth):
+        self.conn = pymysql.connect(
+                host = self.SERVER_IP, 
+                user = "root", 
+                password = "******",
+                database = self.DB_NAME,
+                charset = "utf8"
+            )
         SQL = "INSERT INTO urls (url, md5Code, depth) VALUES (%s, %s, %s);"
         insertData = (url, hashlib.md5(url.encode()).hexdigest(), depth)
         try:
             with self.conn.cursor() as cursor:
                 cursor.execute(SQL, insertData)
         except Exception as identifier:
-            logger.error(identifier)
+            # logger.error("6")
+            # logger.error(identifier)
             self.conn.rollback()
+            return
         else:
             self.conn.commit()
 
     # url出库并加读锁
     def dequeueUrl(self):
+        self.conn = pymysql.connect(
+                host = self.SERVER_IP, 
+                user = "root", 
+                password = "******",
+                database = self.DB_NAME,
+                charset = "utf8"
+            )
         QUERY_SQL = "SELECT id, url, depth FROM urls WHERE status = 'new' ORDER BY id ASC LIMIT 1 FOR UPDATE;"
         UPDATE_SQL = "UPDATE urls SET status = 'downloading' WHERE id = %s;"
         res = None
@@ -60,13 +76,17 @@ class CrawlDataBaseManager:
     def finishUrl(self, id):
         SQL = "UPDATE urls SET status = 'done' WHERE id = %s;"
         try:
+            
             with self.conn.cursor() as cursor:
                 cursor.execute(SQL, id)
         except Exception as identifier:
+            logger.error("7")
             logger.error(identifier)
             self.conn.rollback()
         else:
             self.conn.commit()
+            
+            
 
 if __name__ == "__main__":
     pass
